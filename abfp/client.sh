b0791da197ab637fcfb22a97428cad00847d852e
Rafa Laguna <rafa.laguna@enti.cat>
2021-05-07 08:42:29 +0200

    Multifile

--- abfp/client.sh
+++ abfp/client.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 PORT=2021
+INPUT_PATH="entrada_cliente/"
 
 IP_CLIENT="127.0.0.1"
-IP_SERVER="127.0.0.1"
+
+if [ "$1" == "" ]; then
+	IP_SERVER="127.0.0.1"
+else
+	IP_SERVER="$1"
+fi
 
 echo "Cliente de ABFP"
 
-echo "(2) Sending Headers"
+echo "(2) Sending Headers to $IP_SERVER"
 
 echo "ABFP $IP_CLIENT" | nc -q 1 $IP_SERVER $PORT
 
@@ -23,5 +29,56 @@ echo "(6) HANDSHAKE"
 
 sleep 1
 echo "THIS_IS_MY_CLASSROOM" | nc -q 1 $IP_SERVER $PORT
+
+echo "(7a) LISTEN"
+RESPONSE=`nc -l -p $PORT`
+if [ "$RESPONSE" != "YES_IT_IS" ]; then
+	echo "ERROR: Handshake incorrecto"
+	exit 1
+fi
+
+#ENVIAR NUM ARCHIVOS
+
+echo "(7b) SENDING NUM_FILES"
+sleep 1
+
+NUM_FILES=`ls $INPUT_PATH | wc -w`
+
+echo "NUM_FILES $NUM_FILES" | nc -q 1 $IP_SERVER $PORT
+
+echo "(7c) LISTEN"
+RESPONSE=`nc -l -p $PORT`
+
+if [ "$RESPONSE" != "OK_NUM_FILES" ]; then
+	echo "ERROR: Prefijo NUM_FILES incorrecto"
+	exit 2
+fi
+
+#BUCLE
+
+
+for FILE_NAME in `ls $INPUT_PATH`; do
+
+	FILE_MD5=`echo $FILE_NAME | md5sum | cut -d " " -f 1`
+
+	echo "(10) SENDING FILE_NAME"
+	sleep 1
+	echo "FILE_NAME $FILE_NAME $FILE_MD5" | nc -q 1 $IP_SERVER $PORT
+
+	echo "(11) LISTEN"
+	RESPONSE=`nc -l -p $PORT`
+
+	if [ "$RESPONSE" != "OK_FILE_NAME" ]; then
+		echo "ERROR: envío de archivo fallido"
+
+		exit 2
+	fi
+
+	sleep 1
+	cat $INPUT_PATH$FILE_NAME | nc -q 1 $IP_SERVER $PORT
+done
+
+
+
 
 exit 0
--- abfp/server.sh
+++ abfp/server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PORT=2021
+OUTPUT_PATH="salida_server/"
 
 echo "(0) Server ABFP"
 
@@ -31,6 +32,84 @@ echo "OK_CONN" | nc -q 1 $IP_CLIENT $PORT
 echo  "(5) LISTEN"
 
 HANDSHAKE=`nc -l -p $PORT`
+
+if [ "$HANDSHAKE" != "THIS_IS_MY_CLASSROOM" ]; then
+	echo "ERROR: Handshake incorrecto"
+
+	sleep 1
+	echo "KO_HANDSHAKE" | nc -q 1 $IP_CLIENT $PORT
+
+	exit 2
+fi
+
+echo "(8a) RESPONSE"
+sleep 1
+echo "YES_IT_IS" | nc -q 1 $IP_CLIENT $PORT
+
+
+#LEER NUM ARCHIVOS A RECIBIR
+
+echo "(8b) LISTEN NUM_FILES"
+NUM_FILES=`nc -l -p $PORT`
+
+PREFIX=`echo $NUM_FILES | cut -d " " -f 1`
+NUM=`echo $NUM_FILES | cut -d " " -f 2`
+
+if [ "$PREFIX" != "NUM_FILES" ]; then
+	echo "ERROR: Prefijo NUM_FILES incorrecto"
+
+	sleep 1
+	echo "KO_NUM_FILES" | nc -q 1 $IP_CLIENT $PORT
+
+	exit 2
+fi
+
+sleep 1
+echo "OK_NUM_FILES" | nc -q 1 $IP_CLIENT $PORT
+
+echo "NUM_FILES: $NUM"
+
+#BUCLE
+
+for NUMBER in `seq $NUM`; do
+
+	echo "(9) LISTEN FILE_NAME"
+
+	FILE_NAME=`nc -l -p $PORT`
+
+
+	PREFIX=`echo $FILE_NAME | cut -d " " -f 1`
+	NAME=`echo $FILE_NAME | cut -d " " -f 2`
+	NAME_MD5=`echo $FILE_NAME | cut -d " " -f 3`
+
+	if [ "$PREFIX" != "FILE_NAME" ]; then
+		echo "ERROR: Prefijo FILE_NAME incorrecto"
+
+		sleep 1
+		echo "KO_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+
+		exit 3
+	fi
+
+	TEMP_MD5=`echo $NAME | md5sum | cut -d " " -f 1`
+
+	if [ "$NAME_MD5" != "$TEMP_MD5" ]; then
+		echo "ERROR: MD5 Incorrecto"
+
+		sleep 1
+		echo "KO_FILE_NAME_MD5" | nc -q 1 $IP_CLIENT $PORT
+
+		exit 4
+	fi
+
+	echo "(12) RESPONSE OK_FILE_NAME"
+	sleep 1
+	echo "OK_FILE_NAME" | nc -q 1 $IP_CLIENT $PORT
+
+	echo $OUTPUT_PATH$NAME
+
+	nc -l -p $PORT > $OUTPUT_PATH$NAME
+done
 
 
 exit 0
